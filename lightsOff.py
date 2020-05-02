b6f6f22225222b95768faaec99e92817ea78c1c4
Gregory Maldonado <gmaldona@oswego.edu>
2020-05-01 21:43:00 -0400

    off file

--- lightsOff.py
+++ lightsOff.py
@@ -3,6 +3,12 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)
+
 GPIO.output(23, GPIO.LOW)
 GPIO.output(24, GPIO.LOW)
 GPIO.output(17, GPIO.LOW)

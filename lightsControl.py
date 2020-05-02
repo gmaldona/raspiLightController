3b052dac7fcf588d208541ea72a8d2e0e4551e5b
Gregory Maldonado <gmaldona@oswego.edu>
2020-05-01 21:44:17 -0400

    off file

--- lightsControl.py
+++ lightsControl.py
@@ -1,6 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 
+def blink(pin, t):
+    GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(t)
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(t)
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
@@ -16,10 +23,4 @@ while True:
     blink(27, 1)
     blink(22, 1)
     
-def blink(pin, t):
-    GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.output(pin, GPIO.HIGH)
-    time.sleep(t)
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(t)
     
